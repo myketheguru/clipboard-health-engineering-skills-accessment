6bdebfdfd131daa0b0e8fff77d756ff1625c368b
myketheguru <michael.ezeoda@kuda.com>
2023-05-01 20:40:16 +0100

    Feature: Refactoring and testing

--- dpk.js
+++ dpk.js
@@ -1,28 +1,82 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+// Constants for default partition key and maximum length
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+/**
+ * A helper function to generate a partition key for an event if none exists.
+ * @param {object} event - The event to generate a partition key for.
+ * @returns {string} - The partition key for the event.
+ */
+function generateCandidatePartitionKey(event) {
+  // If event is falsy, return the trivial partition key
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  // If event has a partition key, return it
+  if (event.partitionKey) {
+    return event.partitionKey;
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+
+  // Otherwise, generate a SHA3-512 hash of the event data and return it
+  const data = JSON.stringify(event);
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
+
+/**
+ * A helper function to ensure the event's partition key is a string.
+ * @param {string} candidate - The candidate to generate a partition key for.
+ * @returns {string} - The partition key for the event.
+ */
+function ensurePartitionKeyIsString(candidate) {
+  // If candidate is already a string, return it
+  if (typeof candidate === "string") {
+    return candidate;
   }
-  return candidate;
+
+  // Otherwise, convert it to a JSON string and return it
+  return JSON.stringify(candidate);
+}
+
+/**
+ * A helper function that generates final partition key for an event.
+ * @param {object} candidate - The candidate to generate a final partition key for.
+ * @returns {string} - The partition key for the event.
+ */
+function generateFinalPartitionKey(candidate) {
+  // If the length of the candidate partition key is less than or equal to the maximum length,
+  // return it as is
+  if (candidate.length <= MAX_PARTITION_KEY_LENGTH) {
+    return candidate;
+  }
+
+  // Otherwise, generate a SHA3-512 hash of the candidate partition key and return it
+  return crypto.createHash("sha3-512").update(candidate).digest("hex");
+}
+
+/**
+ * Main function that generates a deterministic partition key from an event
+ * Generates a deterministic partition key for an event.
+ * @param {object} event - The event to generate a partition key for.
+ * @returns {string} - The partition key for the event.
+ */
+function deterministicPartitionKey(event) {
+  // Generate the candidate partition key
+  const candidate = generateCandidatePartitionKey(event);
+
+  // Ensure the candidate partition key is a string
+  const partitionKey = ensurePartitionKeyIsString(candidate);
+
+  // Generate the final partition key
+  const finalPartitionKey = generateFinalPartitionKey(partitionKey);
+
+  // Return the final partition key
+  return finalPartitionKey;
+}
+
+// Export the main function
+module.exports = {
+  deterministicPartitionKey,
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,57 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+  it("returns default partition key when no event is provided", () => {
+    const expected = "0";
+    const actual = deterministicPartitionKey();
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns partition key from event when defined", () => {
+    const event = { partitionKey: "test-key" };
+    const expected = "test-key";
+    const actual = deterministicPartitionKey(event);
+    expect(actual).toEqual(expected);
+  });
+
+  it("generates partition key from event when not defined", () => {
+    const event = { key: "value" };
+    const data = JSON.stringify(event);
+    const hash = crypto.createHash("sha3-512").update(data).digest("hex");
+    const expected = hash;
+    const actual = deterministicPartitionKey(event);
+    expect(actual).toEqual(expected);
+  });
+
+  it("converts candidate partition key to string when not already a string", () => {
+    const event = { key: "value" };
+    const data = JSON.stringify(event);
+    const hash = crypto.createHash("sha3-512").update(data).digest("hex");
+    const candidate = Buffer.from(hash);
+    const expected = hash;
+    const actual = deterministicPartitionKey({ key: candidate });
+    expect(actual).toEqual(expected);
+  });
+
+  it("truncates partition key when too long", () => {
+    const event = { key: "value" };
+    const data = JSON.stringify(event);
+    const hash = crypto.createHash("sha3-512").update(data).digest("hex");
+    const longKey = hash.repeat(100);
+    const expected = crypto
+      .createHash("sha3-512")
+      .update(longKey)
+      .digest("hex");
+    const actual = deterministicPartitionKey({ key: longKey });
+    expect(actual).toEqual(expected);
+  });
+
+  it("generates different partition keys for different events", () => {
+    const event1 = { key1: "value1" };
+    const event2 = { key2: "value2" };
+    const actual1 = deterministicPartitionKey(event1);
+    const actual2 = deterministicPartitionKey(event2);
+    expect(actual1).not.toEqual(actual2);
   });
 });
